0615614adb243ef5a36a96c73a1d143f9e3329f2
kraiponn <kraipon.na10@gmail.com>
2018-07-05 23:00:44 +0700

    add volley activity2

---
+++ app/src/main/java/com/ksn/kraiponn/labdao/activity/VolleyActivity.java
@@ -0,0 +1,31 @@
+package com.ksn.kraiponn.labdao.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.ksn.kraiponn.labdao.R;
+
+public class VolleyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
